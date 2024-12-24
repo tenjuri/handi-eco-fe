14f49edc9d4097cab3bfd846c5d5b0cec14a4993
tuantt <tuantt@cyberk.io>
2024-12-24 23:24:10 +0700

    change: slide

--- src/hooks/useOurProduct.ts
+++ src/hooks/useOurProduct.ts
@@ -17,7 +17,7 @@ const useOurProduct = () => {
         {
           title: "Baby Changing Basket",
           description: [
-            "Our 'EcoCraft' baby change baskets are made with soft organic seagrass, with colors and patterns exclusive to The EcoCraft Collective, adding a unique touch to your home and nursery",
+            "Our 'Handi&Eco' baby change baskets are made with soft organic seagrass, with colors and patterns exclusive to The Handi&Eco Collective, adding a unique touch to your home and nursery",
             "Make a great alternative to a traditional baby change table.",
             "Easily portable, allowing the basket to come to your baby.",
             "These baskets are lightweight, designed to move with ease from room to room, nappy change to nappy change.",
@@ -43,6 +43,35 @@ const useOurProduct = () => {
       name: "Kitchen & Dinning",
       slug: "kitchen-dinning",
       url: "/our-product/kitchen-dinning",
+      content: [
+        {
+          title: "Tray",
+          description: [
+            "Add Handi&Eco's handcrafted Trays to create a highlight to make your space more sophisticated. Not only in the kitchen or living room, this type of tray is very popular for storing food and fruits at parties, serving food on the beach, and on picnics.",
+          ],
+          url: "/",
+          btnLabel: "TRAY CATALOGUE",
+          banner: "https://media.handi-eco.vn/images/kitchen.jpg",
+        },
+        {
+          title: "Placemat",
+          description: [
+            "Placemat at Handi&Eco is a unique, creative handmade work from natural materials such as rattan, seagrass, water hyacinth, and bamboo. With its natural beauty and delicate combination of natural materials, placemat not only protects your dining table and coffee table from scratches and stains, but also makes your meals and afternoon tea more enjoyable. you become beautiful and unique. Therefore, placemats are often used in formal meetings, wedding parties or served in natural-style restaurants, prairies or coastal restaurants. People who really know how to enjoy life often use this type of placemat.",
+          ],
+          url: "/",
+          btnLabel: "PLACEMAT CATALOGUE",
+          banner: "https://media.handi-eco.vn/images/kitchen.jpg",
+        },
+        {
+          title: "Tissue Box",
+          description: [
+            "Tissue boxes at Handi&Eco are designed to keep the tissues inside always dry and mold-free. It can be applied in many office spaces, living rooms, dining rooms, bedrooms, or at parties, restaurants,...",
+          ],
+          url: "/",
+          btnLabel: "TISSUE BOX CATALOGUE",
+          banner: "https://media.handi-eco.vn/images/kitchen.jpg",
+        },
+      ],
     },
     {
       name: "Outdoor & Garden",
@@ -58,6 +87,17 @@ const useOurProduct = () => {
       name: "Bathroom & Laundry",
       slug: "bathroom-laundry",
       url: "/our-product/bathroom-laundry",
+      content: [
+        {
+          title: "Hanging Shelf",
+          description: [
+            "Hanging shelves at Handi&Eco have a unique structure created from natural materials, bringing organic and sustainable beauty to any space. ECOCRAFT's Hanging shelft creates a rustic feel, with natural colors, they also bring a warm feeling. There are many different types of Hanging shelves and they can be used as book shelves in the office, as display shelves in the living room, as fruit shelves in the kitchen, as makeup shelves in the bedroom or as shelves leave things in the bathroom.",
+          ],
+          url: "/",
+          btnLabel: "HANGING SHELF CATALOGUE",
+          banner: "https://media.handi-eco.vn/images/outdoor.jpg",
+        },
+      ],
     },
   ];
   return { ourProducts };
