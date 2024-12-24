0c0245516812fe3e7295beca027738a512dcf063
tuantt <tuantt@cyberk.io>
2024-12-24 21:21:08 +0700

    chore: metadata

--- src/app/material/[slug]/page.tsx
+++ src/app/material/[slug]/page.tsx
@@ -1,37 +1,25 @@
 import React from "react";
-import useCategory from "@/hooks/useCategory";
+import { Metadata } from "next";
 import Material from "@/components/material/page";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const slug = params.slug;
-  const { categories } = useCategory();
-  const category = categories.find((category) => category.slug === slug);
-  return {
-    title: `Handi&Eco - ${category?.name}`,
+export const metadata: Metadata = {
+  title: "Handi&Eco - Material",
+  description: "Handi&Eco - Handmade with Care, Eco for Life",
+  openGraph: {
+    title: "Handi&Eco - Material",
     description: "Handi&Eco - Handmade with Care, Eco for Life",
-    openGraph: {
-      title: "Handi&Eco - Material",
-      description: "Handi&Eco - Handmade with Care, Eco for Life",
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Handi&Eco - Material",
-      description: `Handi&Eco - ${category?.name}`,
-    },
-    robots: {
-      index: true,
-      follow: true,
-    },
-    icons: {
-      shortcut: "/favicon.ico",
-    },
-  };
-}
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Material",
+    description: "Material",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export async function generateStaticParams() {
   return [
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,39 +1,28 @@
 import React from "react";
+import { Metadata } from "next";
 import Product from "@/components/product/page";
 import useProducts from "@/hooks/useProducts";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const slug = params.slug;
-  const { products } = useProducts();
-  const product = products.find((product) => product.slug === slug);
-  return {
-    title: `Handi&Eco - ${product?.name}`,
+export const metadata: Metadata = {
+  title: "Handi&Eco - Product",
+  description: "Handi&Eco - Handmade with Care, Eco for Life",
+  openGraph: {
+    title: "Handi&Eco - Product",
     description: "Handi&Eco - Handmade with Care, Eco for Life",
-    openGraph: {
-      title: "Handi&Eco - Product",
-      description: "Handi&Eco - Handmade with Care, Eco for Life",
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Handi&Eco - Product",
-      description: `Handi&Eco - ${product?.name}`,
-    },
-    robots: {
-      index: true,
-      follow: true,
-    },
-    icons: {
-      shortcut: "/favicon.ico",
-    },
-  };
-}
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Product",
+    description: "Product",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   const { products } = useProducts();
   return products.map((product) => ({ slug: product.slug }));
 }
