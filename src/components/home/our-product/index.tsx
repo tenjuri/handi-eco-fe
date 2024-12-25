334239a457f4e123dbf96c56fe4da4f19d3dd537
tuantt <tuantt@cyberk.io>
2024-12-25 07:57:03 +0700

    .

--- src/components/home/our-product/index.tsx
+++ src/components/home/our-product/index.tsx
@@ -30,40 +30,42 @@ const OurProduct: React.FC = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 bg-[#F5F5F5]">
-      <div className="p-10">
-        <h1 className="text-base xl:text-3xl font-bold italic">
-          OUR <br />
-          PRODUCTS
-        </h1>
-      </div>
-      {products.map((item, index) => (
-        <div key={index} className="relative group">
-          <div className="w-full h-[300px] md:h-[400px] xl:h-[700px] overflow-hidden">
-            <img
-              key={index + "img"}
-              src={item.banner}
-              alt={item.name}
-              width={300}
-              height={700}
-              className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
-            />
-          </div>
-
-          <div className="absolute bottom-0 w-full p-4">
-            <p className="text-white text-base md:text-3xl font-medium italic leading-[100%]">
-              {item.name}
-            </p>
-            <div className="w-16 h-1 bg-white mt-1"></div>
-            <Link
-              href={`/our-product/${item.url}`}
-              className="bg-[#d5b36f] text-white text-base md:text-lg py-1 md:py-2 px-4 rounded-md hover:bg-[#d5b36f]/80 cursor-pointer mt-2 inline-block"
-            >
-              READ MORE
-            </Link>
-          </div>
+    <div className="w-full max-w-[1920px] mx-auto bg-[#F5F5F5]">
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 ">
+        <div className="p-10">
+          <h1 className="text-base xl:text-3xl font-bold italic">
+            OUR <br />
+            PRODUCTS
+          </h1>
         </div>
-      ))}
+        {products.map((item, index) => (
+          <div key={index} className="relative group">
+            <div className="w-full h-[300px] md:h-[400px] xl:h-[700px] overflow-hidden">
+              <img
+                key={index + "img"}
+                src={item.banner}
+                alt={item.name}
+                width={300}
+                height={700}
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+              />
+            </div>
+
+            <div className="absolute bottom-0 w-full p-4">
+              <p className="text-white text-base md:text-3xl font-medium italic leading-[100%]">
+                {item.name}
+              </p>
+              <div className="w-16 h-1 bg-white mt-1"></div>
+              <Link
+                href={`/our-product/${item.url}`}
+                className="bg-[#d5b36f] text-white text-base md:text-lg py-1 md:py-2 px-4 rounded-md hover:bg-[#d5b36f]/80 cursor-pointer mt-2 inline-block"
+              >
+                READ MORE
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
