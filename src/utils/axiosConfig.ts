c12b9cf2095da41a4abdab0f7037f11c67945443
tuantt <tuantt@cyberk.io>
2025-01-08 20:58:18 +0700

    feat: blogs

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
-  timeout: 5000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
