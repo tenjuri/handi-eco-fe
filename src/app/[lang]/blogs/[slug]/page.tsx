fa4f3c488778651072f6a570757e93d39d818432
tuantt <tuantt@cyberk.io>
2025-01-09 19:29:12 +0700

    chore: fix lint

--- src/app/[lang]/blogs/[slug]/page.tsx
+++ src/app/[lang]/blogs/[slug]/page.tsx
@@ -4,49 +4,52 @@ import Blogs from "@/components/blogs";
 import axiosInstance from "@/utils/axiosConfig";
 import { Blog } from "@/model/blog.model";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string; lang: string };
-}): Promise<Metadata> {
-  const { slug } = params;
-  const { data } = await axiosInstance.get<Blog>(`/blogs/${slug}`);
-
-  return {
-    title: `New - ${data.title}`,
+export const metadata: Metadata = {
+  title: "Handi&Eco - News",
+  description: "Handi&Eco - Handmade with Care, Eco for Life",
+  openGraph: {
+    title: "Handi&Eco - News",
     description: "Handi&Eco - Handmade with Care, Eco for Life",
-    openGraph: {
-      title: `New - ${data.title}`,
-      description: "Handi&Eco - Handmade with Care, Eco for Life",
-      url: "https://handi-eco.vn",
-      siteName: "Handi&Eco - Blogs",
-      images: ["https://handi-eco.vn/logo-lg.jpg"],
-      type: "website",
-    },
-    twitter: {
-      title: `New - ${data.title}`,
-      card: "summary_large_image",
-      images: ["https://handi-eco.vn/logo-lg.jpg"],
-      description: "Handi&Eco - Handmade with Care, Eco for Life",
-    },
-    keywords: [
-      "Handmade",
-      "Care",
-      "Eco",
-      "Life",
-      "Eco for Life",
-      "Handmade with Care",
-      "handi eco",
-      "handi & eco",
-      data.title,
-    ],
-  };
+    url: "https://handi-eco.vn",
+    siteName: "Handi&Eco - News",
+    images: ["https://handi-eco.vn/logo-lg.jpg"],
+    type: "website",
+  },
+  twitter: {
+    title: "Handi&Eco - News",
+    card: "summary_large_image",
+    images: ["https://handi-eco.vn/logo-lg.jpg"],
+    description: "Handi&Eco - Handmade with Care, Eco for Life",
+  },
+  keywords: [
+    "Handmade",
+    "Care",
+    "Eco",
+    "Life",
+    "Eco for Life",
+    "Handmade with Care",
+    "handi eco",
+    "handi & eco",
+  ],
+};
+
+export async function generateStaticParams() {
+  try {
+    const blogs = await axiosInstance.get<Blog[]>("/blogs");
+    return blogs.data.map((blog: Blog) => ({
+      slug: blog.slug,
+    }));
+  } catch (error) {
+    console.error("Error generating static params:", error);
+    return [];
+  }
 }
 
 export default async function Page({
   params,
 }: {
-  params: { slug: string; lang: string };
+  params: Promise<{ slug: string }>;
 }) {
-  return <Blogs slug={params.slug} />;
+  const slug = (await params).slug;
+  return <Blogs slug={slug} />;
 }
