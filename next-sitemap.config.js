12796313d383db0f876958961d5867163cdf0bef
tuantt <tuantt@cyberk.io>
2025-01-09 20:06:26 +0700

    feat: change logo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://handi-eco.vn',
+  siteUrl: "https://handi-eco.vn",
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  changefreq: "daily",
+  exclude: ["/admin"],
 };
 
 module.exports = config;
