eeecfecd070b9734f912f8f6d1b45deb2567f7a6
tuantt <tuantt@cyberk.io>
2025-04-28 17:35:56 +0700

    feat: change

--- src/hooks/useOurTeam.ts
+++ src/hooks/useOurTeam.ts
@@ -5,13 +5,6 @@ import OurTeam3 from "@/assets/images/our-team/our-team-3.jpg";
 export const useOurTeam = () => {
   const ourTeams = [
     {
-      name: "Peter Nguyen",
-      position: "CEO & Co-Founder",
-      phone: "+84-34-223-6789",
-      mail: "Peter@handi-eco.vn",
-      image: OurTeam1,
-    },
-    {
       name: "Duc Thien Andree",
       position: "Manager & Co-Founder",
       phone: "+84-823-666-533",
@@ -24,6 +17,13 @@ export const useOurTeam = () => {
       phone: "+84-968-230-564",
       mail: "Elly@handi-eco.vn",
       image: OurTeam3,
+    },
+    {
+      name: "Peter Nguyen",
+      position: "Assistant Manager",
+      phone: "",
+      mail: "Peter@handi-eco.vn",
+      image: OurTeam1,
     },
   ];
   return { ourTeams };
