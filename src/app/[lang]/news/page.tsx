602f2483e2685bbd34927876799e30209ac985fe
tuantt <tuantt@cyberk.io>
2025-04-29 09:05:57 +0700

    feat: update

--- src/app/[lang]/news/page.tsx
+++ src/app/[lang]/news/page.tsx
@@ -1,6 +1,25 @@
+"use client";
 import React from "react";
 import News from "@/components/news";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function NewsPage() {
-  return <News />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+            staleTime: 1000 * 60 * 5, // 5 minutes
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <News />
+    </QueryClientProvider>
+  );
 }
--- src/components/news/index.tsx
+++ src/components/news/index.tsx
@@ -1,38 +1,31 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axiosInstance from "@/utils/axiosConfig";
 import { useRouter } from "next/navigation";
 import { New } from "@/model/new.model";
 import Image from "next/image";
 import { Skeleton } from "antd";
+import { useQuery } from "@tanstack/react-query";
+
 const News: React.FC = () => {
   const router = useRouter();
-  const [news, setNews] = useState<New[]>([]);
-  const [loading, setLoading] = useState(true);
 
-  const getNews = async () => {
-    try {
-      const res = await axiosInstance.get("/news/all");
-      setNews(res.data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching news:", error);
-      setLoading(false);
-    }
-  };
-  useEffect(() => {
-    getNews();
-  }, []);
+  // Fetch news data using react-query
+  const { data, isFetching } = useQuery({
+    queryKey: ["news"],
+    queryFn: () =>
+      axiosInstance.get("/news/all").then((res) => res.data as New[]),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    gcTime: 1000 * 60 * 5, // 5 minutes
+  });
 
   return (
     <div className="w-full max-w-[1440px] mx-auto mt-6 px-6 xl:px-5">
       <p className="text-2xl font-bold">News</p>
-      {loading ? (
+      {isFetching ? (
         <Skeleton active className="mt-7" />
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-7">
-          {news.map((item) => (
+          {data?.map((item) => (
             <div
               key={item.id}
               className="border border-gray-200 rounded-lg p-4 cursor-pointer shadow-md group"
